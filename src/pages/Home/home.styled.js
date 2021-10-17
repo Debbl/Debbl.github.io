32da17a79949186cfcb91a4504e14b1cfdee481b
Debbl <1685294601@qq.com>
2021-10-17 20:50:16 +0800

    :sparkles: Add Find You

--- src/pages/Home/home.styled.js
+++ src/pages/Home/home.styled.js
@@ -3,8 +3,31 @@ import styled from "styled-components";
 const HomeWrapper = styled.div`
   display: flex;
   height: 80vh;
+  width: 100%;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
+
+  .one-wrap {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .two-wrap {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .two-wrap::after {
+      content: "";
+      display: inline-block;
+      width: 200px;
+      height: 100px;
+  }
 `
 
 export {
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,10 +6,16 @@ import ShowItem from '../../components/show-item'
 function Home() {
   return (
     <HomeWrapper>
-      <ShowItem item_link="https://blog.aiwan.run/"
-                item_des="My Blog" />
-      <ShowItem item_link="https://tools.aiwan.run/"
-                item_des="WebTools" />
+      <div className="one-wrap">
+        <ShowItem item_link="https://blog.aiwan.run/"
+                  item_des="My Blog" />
+        <ShowItem item_link="https://tools.aiwan.run/"
+                  item_des="WebTools" />
+      </div>
+      <div className="two-wrap">
+        <ShowItem item_link="https://u.aiwan.run/"
+        item_des="Find You" />
+      </div>
     </HomeWrapper>
   )
 }
